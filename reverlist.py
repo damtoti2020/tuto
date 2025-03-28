4bea9e5075101618434d598ac138fec26c56d20f
damtoti <richegod@gmail.com>
2025-03-28 17:12:14 +0100

    Create reverlist.py

---
+++ reverlist.py
@@ -0,0 +1,15 @@
+import os
+liste1 = [8, 9, 3, 6, 1, 10]
+liste1.reverse()
+print("The reversed list is", liste1)
+
+liste2 = [91, 67, 120, 34, 76, 54, 78, 87, 56, 64, 345]
+liste2.sort()
+print("The sorted list is", liste2)
+
+liste3 = []
+liste3 = liste1.copy()
+print("liste3 =", liste3)
+
+indexvalue =liste2[2.6]
+print("The index value are" , indexvalue)
